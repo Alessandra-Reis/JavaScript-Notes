c19603e219b3a3d5d6fc6e5a6b057fb3fcb14ef5
Alessandra Reis <alereis1002@gmail.com>
2023-07-22 20:03:31 +0100

    Initial commit

---
+++ JavaScript/notes.js
@@ -0,0 +1,19 @@
+/*  
+    *****************
+    JAVA SCRIPT NOTES
+    *****************
+*/
+
+/* 
+
+JavaScript's main purpose as a scripting language is to take HTML and make it dynamic.
+
+DOMAINS: 
+
+1 - JavaScript Operators, Methods, and Keywords
+2 - Variables, Data Types, and Functions
+3 - Decisions and Loops
+4 - Document Object Model
+5 - HTML Forms
+
+*/
---
+++ Primer/app.js
@@ -0,0 +1,16 @@
+console.log('hi');
+
+//comment line
+
+let x = 7;
+let y = 3;
+let z = x + y;
+console.log('Answer: ' + z);
+
+/*
+Case Sensitivity
+
+All global variables that are accessible anywhere in a program are set to all uppercase, such as HIGHSCORE.
+Temporary variables used in loops are single letters in lowercase, such asj.
+Function names use a capital letter at the start of each word, like this:MyFunctionName().
+*/
